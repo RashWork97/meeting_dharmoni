9f58f10a93393a38c6bf1a66b5d3af73503a7013
RashidWork97 <rashideen.work@gmail.com>
2026-06-18 17:20:26 +0800

    Added logic for time slot options and masih dalam adjustment untuk block duration.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,50 @@
 import './bootstrap';
+
+const time_slots = [
+    { name: 'time1', value: '8:30am' },
+    { name: 'time2', value: '9:00am' },
+    { name: 'time3', value: '9:30am' },
+    { name: 'time4', value: '10:00am' },  
+    { name: 'time5', value: '10:30am' },  
+    { name: 'time6', value: '11:00am' },  
+    { name: 'time7', value: '11:30am' },  
+    { name: 'time8', value: '12:00pm' },  
+    { name: 'time9', value: '12:30pm' }
+];
+
+const blockedTime = [];
+
+window.checkEndTime = function (){
+    const start_time_choosen = document.getElementById('start_time').value;
+    const show_end_time = document.getElementById('end_time');
+
+    show_end_time.innerHTML = '<option value="" selected disabled> Choose end time</option>';
+
+    for (const time_slot of time_slots){
+        if(start_time_choosen < time_slot.name){
+            const end_time = document.createElement('option');
+            end_time.value = time_slot.name;
+            end_time.textContent = time_slot.value;
+            show_end_time.appendChild(end_time);
+        }
+    }
+    
+
+};
+
+window.blockedDuration = function(){
+    const start_time_choosen = document.getElementById('start_time').value;
+    const show_end_time = document.getElementById('end_time').value;
+    const start_text = time_slots.find(slot => slot.name === start_time_choosen);
+    const end_text = time_slots.find(slot => slot.name === show_end_time);
+
+    for(const slot of time_slots){
+        if(slot.name === start_time_choosen){
+            console.log(slot.value);
+            if(slot.name < show_end_time){
+                console.log(slot.value);
+            }
+        }
+    }
+}
+
--- resources/views/main/application.blade.php
+++ resources/views/main/application.blade.php
@@ -4,6 +4,7 @@
 @endsection
 
 @section('main_content')
+@vite('resources/js/app.js')
  <div>
     <div class='bg-amber-100 shadow-sm shadow-blue-400'>
         <h1>
@@ -16,21 +17,50 @@
 
         <form>
             <div class='border border-amber-950'>
-                <label for="date"> Please choose a date:
-                    <input type="date" name='date'>
+                <label for="date_book"> Please choose a date:
+                    <input type="date" name='date_book' id="date_book">
                 </label>
                 <br>
                 <br>
-                <label for="timeslot">
-                    Please choose time slot:
-                    <input type="time">
+                <label for="start_time">
+                    Please choose start time:
+                    <select for="start_time" name="start_time" id="start_time" onchange="checkEndTime()">
+                        <option value="" selected disabled>Choose start time</option>
+                        <option value="time1"> 8:30am </option>
+                        <option value="time2"> 9:00am</option>
+                        <option value="time3"> 9:30am</option>
+                        <option value="time4"> 10:00am</option>
+                        <option value="time5"> 10:30am</option>
+                        <option value="time6"> 11:00am</option>
+                        <option value="time7"> 11:30am</option>
+                        <option value="time8"> 12:00pm</option>
+                        <option value="time9"> 12:30pm</option>
+                    </select>
+                </label>
+                <br>
+                <br>
+                <label for="end_time">
+                    Please choose end time:
+                    <select for="end_time" name="end_time" id="end_time" onchange="blockedDuration()">
+                        <option value="" selected disabled> Choose end time</option>
+                        <!-- <option value="time1"> 8:30am </option>
+                        <option value="time2"> 9:00am</option>
+                        <option value="time3"> 9:30am</option>
+                        <option value="time4"> 10:00am</option>
+                        <option value="time5"> 10:30am</option>
+                        <option value="time6"> 11:00am</option>
+                        <option value="time7"> 11:30am</option>
+                        <option value="time8"> 12:00pm</option>
+                        <option value="time9"> 12:30pm</option> -->
+                    </select>
                 </label>
 
                 <br>
                 <br>
-                <label for="timeslot">
+                <label for="meetingroom">
                     Please choose meeting room:
-                    <select name="meetingroom" id="meetingroom">
+                    <select for="meetingroom" name="meetingroom" id="meetingroom">
+                        <option value="" selected disabled>Choose meeting room</option>
                         <option value="room1"> Meeting Room 1</option>
                         <option value="room2"> Meeting Room 2</option>
                         <option value="room3"> Meeting Room 3</option>
@@ -39,16 +69,16 @@
                 
                 <br>
                 <br>
-                <label for="duration">
+                <!-- <label for="duration">
                     Please choose duration:
-                    <input type="number">
+                    <input type="number" name="duration" id="duration">
                 </label>
 
                 <br>
-                <br>
+                <br> -->
                 <label for="no_pax">
                     Please put number of pax:
-                    <input type="number">
+                    <input type="number" name="no_pax" id="no_pax" placeholder="Please put no of pax">
                 </label>
 
 
